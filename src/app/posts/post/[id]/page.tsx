dad1280e6a639df26116a56c0baa9d6896a50aba
Patryk Babula <me.morte03@gmail.com>
2023-12-11 14:43:32 +0100

    chore: add date formatting

--- src/app/posts/post/[id]/page.tsx
+++ src/app/posts/post/[id]/page.tsx
@@ -1,14 +1,17 @@
+import dayjs from 'dayjs';
+
 import DeleteButton from './components/DeleteButton';
 import { getPost } from '@/api';
 
 export default async function Post({ params }: { params: { id: string } }) {
   const post = await getPost(+params.id);
+  const formattedDate = dayjs(post.createdAt).format('D of MMMM YYYY');
 
   return (
     <div>
       <DeleteButton id={params.id} />
       <h1>{post.title}</h1>
-      <span>{post.createdAt}</span>
+      <span>{formattedDate}</span>
       <p>{post.description}</p>
     </div>
   );
